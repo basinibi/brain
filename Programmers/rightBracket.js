880de8527ca0a0c18e663d25ccfa82a2682e0116
박석현 <shpark@innogrid.com>
2023-01-09 20:26:22 +0900

    algorithm

---
+++ Programmers/rightBracket.js
@@ -0,0 +1,46 @@
+// https://school.programmers.co.kr/learn/courses/13213/lessons/91074
+
+/*
+문제 설명 - 스택 문제
+괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+
+"()()" 또는 "(())()" 는 올바른 괄호입니다.
+")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+'(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+
+제한사항
+문자열 s의 길이 : 100,000 이하의 자연수
+문자열 s는 '(' 또는 ')' 로만 이루어져 있습니다.
+
+Example
+s	     |  answer
+"()()"	 |   true
+"(())()" |	 true
+")()("	 |   false
+"(()("	 |   false
+
+*/
+
+function solution(s){
+
+    let stack = [];
+
+    for (let char of s) {
+        if (stack.length === 0 && char === ")") { // 처음 또는 빈 스택일 경우 ( 가 들어오는 3번 예 같은 케이스를 예방
+            return false
+        } else {
+            if (char === "(") {
+                stack.push("(");
+            } else {
+                stack.pop();
+            }
+        }
+    }
+
+    return stack.length === 0; // 빈 스택이 아니면
+}
+
+console.log(solution("()()"))
+console.log(solution("(())()"))
+console.log(solution(")()("))
+console.log(solution("(()("))
